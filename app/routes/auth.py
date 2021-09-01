632402b6c44ebc44821bcdf7a9f95597cb4737d0
ORANZINO <hupajupa61@gmail.com>
2021-09-01 12:12:27 +0900

    [UPDATE] auth API 리팩토링

--- app/routes/auth.py
+++ app/routes/auth.py
@@ -3,16 +3,15 @@ from datetime import datetime, timedelta
 import bcrypt
 import base64
 import jwt
-from fastapi import APIRouter, Depends, Header
+from fastapi import APIRouter, Depends, Header, Body
 
-# TODO:
 from sqlalchemy.orm import Session
 from starlette.responses import JSONResponse
 
 from app.common.consts import JWT_SECRET, JWT_ALGORITHM
 from app.database.conn import db
 from app.database.schema import Users, Characters
-from app.models import SnsType, Token, UserToken, UserRegister, UserLogin
+from app.models import SnsType, Token, UserToken, UserRegister, UserLogin, Email, UserName, CharacterName, Duplicated
 
 """
 1. 구글 로그인을 위한 구글 앱 준비 (구글 개발자 도구)
@@ -45,25 +44,28 @@ from app.models import SnsType, Token, UserToken, UserRegister, UserLogin
 router = APIRouter(prefix="/auth")
 
 
-@router.get("/dup_email")
-async def check_email(email: str = Header(None), session: Session = Depends(db.session)):
-    user = Users.get(session, email=email)
-    return JSONResponse(status_code=200, content=dict(msg="CHECK_EMAIL_SUCCESS", result=bool(user)))
+@router.post("/email/check", status_code=200, response_model=Duplicated)
+async def check_email(email: Email = Body(...), session: Session = Depends(db.session)):
+    user = Users.get(session, email=email.email)
+    return {"duplicated": bool(user)}
 
 
-@router.get('/dup_user_name')
-async def check_user_name(user_name: str = Header(None), session: Session = Depends(db.session)):
-    user = Users.get(session, name=str(base64.b64decode(user_name.encode()), encoding='utf-8'))
-    return JSONResponse(status_code=200, content=dict(msg="CHECK_CHARACTER_NAME_SUCCESS", result=bool(user)))
+@router.post('/user/check', status_code=200)
+async def check_user_name(user_name: UserName = Body(...), session: Session = Depends(db.session)):
+    user = Users.get(session, name=user_name.user_name)
+    return {"duplicated": bool(user)}
 
 
-@router.get('/dup_character_name')
-async def check_character_name(character_name: str = Header(None), session: Session = Depends(db.session)):
-    character = Characters.get(session, name=str(base64.b64decode(character_name.encode()), encoding='utf-8'))
-    return JSONResponse(status_code=200, content=dict(msg="CHECK_CHARACTER_NAME_SUCCESS", result=bool(character)))
+@router.post('/character/check', status_code=200)
+async def check_character_name(character_name: CharacterName = Body(...), session: Session = Depends(db.session)):
+    character = Characters.get(session, name=character_name.character_name)
+    return {"duplicated": bool(character)}
 
 
-@router.post("/register/{sns_type}", status_code=201, response_model=Token)
+@router.post("/register/{sns_type}", status_code=201, response_model=Token, responses={
+    400: dict(msg="EMAIL_EXISTS"),
+    404: dict(msg="NOT_SUPPORTED")
+})
 async def register(sns_type: SnsType, reg_info: UserRegister, session: Session = Depends(db.session)):
     """
     `회원가입 API`\n
@@ -74,23 +76,22 @@ async def register(sns_type: SnsType, reg_info: UserRegister, session: Session =
     """
     if sns_type == SnsType.email:
         is_exist = await is_email_exist(reg_info.email)
-        if not reg_info.email or not reg_info.pw:
-            return JSONResponse(status_code=400, content=dict(msg="Email and PW must be provided"))
         if is_exist:
             return JSONResponse(status_code=400, content=dict(msg="EMAIL_EXISTS"))
         hash_pw = bcrypt.hashpw(reg_info.pw.encode("utf-8"), bcrypt.gensalt())
         new_user = Users.create(session, auto_commit=True, pw=hash_pw, email=reg_info.email, name=reg_info.name, profile_img=reg_info.profile_img, sns_type=SnsType.email)
         token = dict(Authorization=f"Bearer {create_access_token(data=UserToken.from_orm(new_user).dict(exclude={'pw', 'marketing_agree'}),)}")
         return token
-    return JSONResponse(status_code=400, content=dict(msg="NOT_SUPPORTED"))
+    return JSONResponse(status_code=404, content=dict(msg="NOT_SUPPORTED"))
 
 
-@router.post("/login/{sns_type}", status_code=200, response_model=Token)
-async def login(sns_type: SnsType, user_info: UserLogin):
+@router.post("/login/{sns_type}", status_code=200, response_model=Token, responses={
+    400: dict(msg="NO_MATCH_USER"),
+    404: dict(msg="NOT_SUPPORTED")
+})
+async def login(sns_type: SnsType, user_info: UserLogin = Body(...)):
     if sns_type == SnsType.email:
         is_exist = await is_email_exist(user_info.email)
-        if not user_info.email or not user_info.pw:
-            return JSONResponse(status_code=400, content=dict(msg="Email and PW must be provided"))
         if not is_exist:
             return JSONResponse(status_code=400, content=dict(msg="NO_MATCH_USER"))
         user = Users.get(email=user_info.email)
@@ -116,3 +117,4 @@ def create_access_token(*, data: dict = None, expires_delta: int = None):
         to_encode.update({"exp": datetime.utcnow() + timedelta(hours=expires_delta)})
     encoded_jwt = jwt.encode(to_encode, JWT_SECRET, algorithm=JWT_ALGORITHM)
     return encoded_jwt
+
