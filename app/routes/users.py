ad81e89a47853fc0d21b196d7dcaa955d955cbf1
ORANZINO <hupajupa61@gmail.com>
2021-09-01 13:21:15 +0900

    [UPDATE] User API 리팩토링

--- app/routes/users.py
+++ app/routes/users.py
@@ -6,30 +6,34 @@ from starlette.responses import JSONResponse
 from app.database.conn import db
 from app.database.schema import Users
 
-from app.models import UserMe, UserUpdate
+from app.models import UserMe, UserUpdate, Message
 
 router = APIRouter(prefix='/user')
 
 
-@router.get('/me')
+@router.get('/', status_code=200, response_model=UserMe, responses={
+    400: dict(description="Given user doesn't exist", model=Message)
+})
 async def get_me(request: Request):
     user = request.state.user
     user_info = Users.get(id=user.id)
-    return JSONResponse(status_code=200, content=dict(msg="READ_USER_SUCCESS", user=UserMe.from_orm(user_info).dict()))
+    if not user_info:
+        return JSONResponse(status_code=400, content=dict(msg="NO_MATCH_USER"))
+    return JSONResponse(status_code=200, content=UserMe.from_orm(user_info).dict())
 
 
-@router.put('/me')
-async def put_me(request: Request, info: UserUpdate, session: Session = Depends(db.session)):
+@router.patch('/', status_code=204, description="Successfully updated user")
+async def update_me(request: Request, info: UserUpdate, session: Session = Depends(db.session)):
     user = request.state.user
     res = Users.filter(session, id=user.id).update(True, **dict(info))
-    return JSONResponse(status_code=200, content=dict(msg="UPDATE_USER_SUCCESS"))
+    return JSONResponse(status_code=204)
 
 
-@router.delete('/me', status_code=200)
+@router.delete('/', status_code=204, description="Successfully deleted user")
 async def delete_me(request: Request):
     user = request.state.user
     Users.filter(id=user.id).delete(auto_commit=True)
-    return JSONResponse(status_code=200, content=dict(msg="DELETE_USER_SUCCESS"))
+    return JSONResponse(status_code=204)
 
 
 
