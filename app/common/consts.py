02bb7ed48d82f4293cfbaf34ae422eee508dd2a8
ORANZINO <hupajupa61@gmail.com>
2021-09-03 15:11:39 +0900

    [UPDATE] post, character GET 시 토큰 필요 없도록 수정

--- app/common/consts.py
+++ app/common/consts.py
@@ -2,4 +2,5 @@ JWT_SECRET = "ABCD1234!"
 JWT_ALGORITHM = "HS256"
 EXCEPT_PATH_LIST = ["/", "/openapi.json"]
 EXCEPT_PATH_REGEX = "^(/docs|/redoc|/auth|/search)"
+EXCEPT_PATH_REGEX_GET = "^(/post|/character)"
 
