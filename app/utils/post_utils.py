c93ea5ac86c28690b7a549a33d9347e46ae082bb
ORANZINO <hupajupa61@gmail.com>
2021-09-04 16:43:46 +0900

    [UPDATE] post 관련 API 통합 및 comment 처리 로직 분리

--- app/utils/post_utils.py
+++ app/utils/post_utils.py
@@ -1,32 +1,33 @@
-from app.database.schema import Characters, PostSunshines, Images, Tracks, Albums, Diaries, Lists, ListComponents
-from app.models import TemplateType, PostRow, CharacterMini
+from collections import defaultdict
+from app.database.schema import Characters, PostSunshines, Images, Tracks, Albums, Diaries, Lists, ListComponents, \
+    CommentSunshines, Comments
+from app.models import PostRow, CharacterMini, CommentMini
 from datetime import timedelta
 
 
-def process_post(character_id, post, session):
+def process_post(session, character_id, post):
     post = PostRow.from_orm(post).dict()
     post['created_at'] = (post['created_at'] + timedelta(hours=9)).isoformat()
     post['updated_at'] = (post['updated_at'] + timedelta(hours=9)).isoformat()
-    character_info = CharacterMini.from_orm(Characters.get(session, id=post['character_id'])).dict()
-    post['character_name'] = character_info['name']
-    post['character_img'] = character_info['profile_img']
+    post['character_info'] = CharacterMini.from_orm(Characters.get(session, id=post['character_id'])).dict()
     my_sunshine = False
     if character_id is not None:
         my_sunshine = bool(PostSunshines.get(session, post_id=post['id'], character_id=character_id))
     post['liked'] = my_sunshine
-    post['type'] = post['template']
+    template_type = post['template']
     post['template'] = dict()
-    if post['type'] == TemplateType.image:
+    post['template']['type'] = template_type
+    if template_type == "Image":
         img = Images.get(session, post_id=post['id']).img
         post['template']['img'] = img
-    elif post['type'] == TemplateType.diary:
+    elif template_type == "Diary":
         diary = Diaries.get(session, post_id=post['id'])
         post['template']['title'] = diary.title
         post['template']['weather'] = diary.weather
         post['template']['img'] = diary.img
         post['template']['date'] = diary.date
         post['template']['content'] = diary.content
-    elif post['type'] == TemplateType.album:
+    elif template_type == "Album":
         album = Albums.get(session, post_id=post['id'])
         post['template']['title'] = album.title
         post['template']['img'] = album.img
@@ -35,7 +36,7 @@ def process_post(character_id, post, session):
         post['template']['release_date'] = album.release_date
         tracks = Tracks.filter(session, album_id=album.id).all()
         post['template']['tracks'] = [{"title": track.title, "lyric": track.lyric} for track in tracks]
-    elif post['type'] == TemplateType.list:
+    elif template_type == "List":
         list_ = Lists.get(session, post_id=post['id'])
         post['template']['title'] = list_.title
         post['template']['content'] = list_.content
@@ -44,3 +45,34 @@ def process_post(character_id, post, session):
         post['template']['components'] = [{"title": component.title, "img": component.img, "content": component.content}
                                           for component in list_components]
     return post
+
+
+def process_comment(session, post_id, character_id):
+    my_comment_sunshine = CommentSunshines.filter(session, post_id=post_id, character_id=character_id).all()
+    my_comment_sunshine = [m.comment_id for m in my_comment_sunshine]
+    parent_comments = session.query(Characters, Comments).filter(Comments.post_id == post_id, Comments.parent == 0) \
+        .join(Characters.comment).order_by('created_at').all()
+    parent_comments = [dict(CommentMini.from_orm(p[1]).dict(), character_info=CharacterMini.from_orm(p[0]).dict()) for p in
+                       parent_comments]
+    parent_ids = list(set(p['id'] for p in parent_comments))
+
+    child_comments = session.query(Characters, Comments).filter(Comments.post_id == post_id,
+                                                                Comments.parent.in_(parent_ids)) \
+        .join(Characters.comment).order_by('created_at').all()
+    child_comments = [dict(CommentMini.from_orm(c[1]).dict(), character_info=CharacterMini.from_orm(c[0]).dict()) for c in
+                      child_comments]
+    children = defaultdict(list)
+    for c in child_comments:
+        children[c['parent']].append(c)
+    for i, p in enumerate(parent_comments):
+        parent_comments[i]['liked'] = True if p['id'] in my_comment_sunshine else False
+        parent_comments[i]['created_at'] = (p['created_at'] + timedelta(hours=9)).isoformat()
+        parent_comments[i]['updated_at'] = (p['updated_at'] + timedelta(hours=9)).isoformat()
+        parent_comments[i]['children'] = children[p['id']]
+        for j, c in enumerate(children[p['id']]):
+            parent_comments[i]['children'][j]['liked'] = True if c['id'] in my_comment_sunshine else False
+            parent_comments[i]['children'][j]['created_at'] = (c['created_at'] + timedelta(hours=9)).isoformat()
+            parent_comments[i]['children'][j]['updated_at'] = (c['updated_at'] + timedelta(hours=9)).isoformat()
+
+    return parent_comments
+
