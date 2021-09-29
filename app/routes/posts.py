eeb86ce7f483dde4936ebad82665dd22c64d3ed6
ORANZINO <hupajupa61@gmail.com>
2021-09-29 22:01:30 +0900

    [UPDATE] Post 관련 API들 token에 맞춰 수정

--- app/routes/posts.py
+++ app/routes/posts.py
@@ -1,12 +1,12 @@
 
-from fastapi import APIRouter, Depends, Header, Body
+from fastapi import APIRouter, Depends, Header, Body, Request
 from sqlalchemy.orm import Session
 from starlette.responses import JSONResponse
 from app.utils.post_utils import process_post, process_comment
 from app.database.conn import db
-from app.database.schema import Posts, Images, Albums, Diaries, Lists, ListComponents, Tracks, Comments, Characters
+from app.database.schema import Posts, Images, Albums, Diaries, Lists, ListComponents, Tracks, Comments, Characters, PostSunshines
 
-from app.models import Post, Comment, PostResponseWithComments, ID, Message, PostList
+from app.models import Post, Comment, PostResponseWithComments, Message, ID, PostListWithNum
 from app.utils.examples import get_post_responses, create_post_requests
 
 router = APIRouter(prefix='/post')
@@ -51,7 +51,10 @@ async def create_post(post: Post = Body(..., examples=create_post_requests), ses
 
 
 @router.post('/comment', status_code=201, response_model=ID)
-async def create_comment(comment: Comment, session: Session = Depends(db.session)):
+async def create_comment(request: Request, comment: Comment, session: Session = Depends(db.session)):
+    user = request.state.user
+    comment = dict(comment)
+    comment['character_id'] = user.default_character_id
     new_comment = Comments.create(session, True, **dict(comment))
     return JSONResponse(status_code=201, content=dict(id=new_comment.id))
 
@@ -59,20 +62,26 @@ async def create_comment(comment: Comment, session: Session = Depends(db.session
 @router.delete('/comment/{comment_id}', status_code=204, responses={
     400: dict(description="Not your comment", model=Message)
 })
-async def delete_comment(comment_id: int, session: Session = Depends(db.session)):
-    Comments.filter(session, id=comment_id).delete(auto_commit=True)
-    Comments.filter(session, parent=comment_id).delete(auto_commit=True)
+async def delete_comment(request: Request, comment_id: int, session: Session = Depends(db.session)):
+    user = request.state.user
+    comment = Comments.get(session, id=comment_id)
+    if comment.character_id == user.default_character_id:
+        Comments.filter(session, id=comment_id).delete(auto_commit=True)
+        Comments.filter(session, parent=comment_id).delete(auto_commit=True)
+    else:
+        return JSONResponse(status_code=400, content=dict(msg="WRONG_CHARACTER"))
     return JSONResponse(status_code=204)
 
 
 @router.get('/{post_id}', status_code=200, response_model=PostResponseWithComments, responses=get_post_responses)
-async def get_post(post_id: int, character_id: int = Header(None), session: Session = Depends(db.session)):
-    post = process_post(session, character_id, Posts.get(session, id=post_id))
-    post['comments'] = process_comment(session, post_id, character_id)
+async def get_post(request: Request, post_id: int, session: Session = Depends(db.session)):
+    user = request.state.user
+    post = process_post(session, user.default_character_id, Posts.get(session, id=post_id))
+    post['comments'] = process_comment(session, post_id, user.default_character_id)
     return JSONResponse(status_code=200, content=post)
 
 
-@router.get('/character/{character_name}/{page_num}', status_code=200, response_model=PostList, responses={
+@router.get('/character/{character_name}/{page_num}', status_code=200, response_model=PostListWithNum, responses={
     404: dict(description="No such character", model=Message)
 })
 async def get_character_posts(character_name: str, page_num: int, session: Session = Depends(db.session)):
