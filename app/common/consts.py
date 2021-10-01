5cc4524c09bd6a9ab47b82f7e44c82ed398b6965
ORANZINO <hupajupa61@gmail.com>
2021-10-01 12:58:41 +0900

    [UPDATE] image upload 로그인 필요없도록

--- app/common/consts.py
+++ app/common/consts.py
@@ -1,6 +1,6 @@
 JWT_SECRET = "ABCD1234!"
 JWT_ALGORITHM = "HS256"
-EXCEPT_PATH_LIST = ["/", "/openapi.json"]
+EXCEPT_PATH_LIST = ["/", "/openapi.json", "/img/upload"]
 EXCEPT_PATH_REGEX = "^(/docs|/redoc|/auth|/search|/[0-9]+)"
 EXCEPT_PATH_REGEX_GET = "^(/post|/character)"
 
