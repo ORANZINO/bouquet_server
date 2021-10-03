b860548d5dee1fd610519340c98968e01fcef2f9
ORANZINO <hupajupa61@gmail.com>
2021-10-04 02:34:38 +0900

    [UPDATE] update notification row model for follow case

---
+++ app/migrations/versions/5d48ade10a9a_1_0_0.py
@@ -0,0 +1,25 @@
+"""1.0.0
+
+Revision ID: 5d48ade10a9a
+Revises: 
+Create Date: 2021-09-26 16:33:20.365135
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '5d48ade10a9a'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
+
