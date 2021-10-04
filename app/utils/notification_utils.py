26452adbfc6cad0cc84189d379e2d5c663608285
ORANZINO <hupajupa61@gmail.com>
2021-10-04 14:36:04 +0900

    [UPDATE] 자기가 자기한테 알림 보내는 일이 없도록 수정

--- app/utils/notification_utils.py
+++ app/utils/notification_utils.py
@@ -54,13 +54,14 @@ def generate_message(token, sender, category, created_at, post_id=None):
 
 def send_notification(sender_id: int, receiver_id: int, category: str, post_id: Optional[int] = None,
                       session: Session = Depends(db.session)):
-    sender, receiver = Characters.get(session, id=sender_id), Characters.get(session, id=receiver_id)
-    token = PushTokens.get(session, user_id=receiver.user_id).token
-    new_notification = Notifications.create(session, True, sender_id=sender_id, receiver_id=receiver_id, category=category, post_id=post_id)
-    if token:
-        response = PushClient().publish(
-            PushMessage(**generate_message(
-                token, sender, category, (new_notification.created_at + timedelta(hours=9)).isoformat(), post_id)))
-        response.validate_response()
+    if sender_id != receiver_id:
+        sender, receiver = Characters.get(session, id=sender_id), Characters.get(session, id=receiver_id)
+        token = PushTokens.get(session, user_id=receiver.user_id).token
+        new_notification = Notifications.create(session, True, sender_id=sender_id, receiver_id=receiver_id, category=category, post_id=post_id)
+        if token:
+            response = PushClient().publish(
+                PushMessage(**generate_message(
+                    token, sender, category, (new_notification.created_at + timedelta(hours=9)).isoformat(), post_id)))
+            response.validate_response()
 
 
