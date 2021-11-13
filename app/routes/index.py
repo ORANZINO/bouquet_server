9baa2b4e82153cc879e8f582976e5a5e84fa7afb
ORANZINO <hupajupa61@gmail.com>
2021-11-14 07:54:48 +0900

    [CREATE] img 적용 API 얼굴 인식 알고리즘 추가 + feed 최신 게시물 필터링 추가

--- app/routes/index.py
+++ app/routes/index.py
@@ -7,7 +7,7 @@ from exponent_server_sdk import (
     PushTicketError,
 )
 from starlette.responses import Response, JSONResponse
-from inspect import currentframe as frame
+from datetime import timedelta
 from fastapi import APIRouter, Depends, Header
 from sqlalchemy.orm import Session
 from starlette.requests import Request
@@ -15,7 +15,7 @@ from typing import Optional
 from app.database.conn import db
 from app.database.schema import Posts, Follows, PushTokens
 from app.utils.post_utils import process_post
-from app.utils.block_utils import block_characters
+from app.utils.notification_utils import send_notification
 from app.models import PostList
 from app.middlewares.token_validator import token_decode
 
@@ -26,18 +26,20 @@ router = APIRouter()
 async def index(page_num: Optional[int] = Header(1), token: Optional[str] = Header(None),
                 session: Session = Depends(db.session)):
     if token is None:
-        posts = session.query(Posts).order_by(Posts.num_sunshines.desc()).offset((page_num - 1) * 10).limit(10).all()
+        posts = session.query(Posts).filter(Posts.created_at > (datetime.now() - timedelta(hours=72)))\
+            .order_by(Posts.num_sunshines.desc()).offset((page_num - 1) * 10).limit(10).all()
         character_id = None
     else:
         user = await token_decode(access_token=token)
         character_id = user['default_character_id']
         followees = Follows.filter(session, follower_id=character_id).all()
         followees = [f.character_id for f in followees]
-        posts = session.query(Posts).filter(Posts.character_id.in_(followees)).order_by(Posts.created_at.desc()) \
-            .offset((page_num - 1) * 10).limit(10).all()
+        posts = session.query(Posts).filter(Posts.created_at > (datetime.now() - timedelta(hours=72)))\
+            .filter(Posts.character_id.in_(followees)).order_by(Posts.created_at.desc()).offset((page_num - 1) * 10).limit(10).all()
+
         if len(posts) < 10:
-            posts += session.query(Posts).filter(Posts.character_id.not_in(followees)).order_by(
-                Posts.created_at.desc()) \
+            posts += session.query(Posts).filter(Posts.created_at > (datetime.now() - timedelta(hours=72)))\
+                .filter(Posts.character_id.not_in(followees)).order_by(Posts.created_at.desc())\
                 .offset((page_num - 1) * 10).limit(10 - len(posts)).all()
 
     posts = [process_post(session, character_id, post) for post in posts]
@@ -47,17 +49,13 @@ async def index(page_num: Optional[int] = Header(1), token: Optional[str] = Head
 
 @router.get("/test")
 async def test(request: Request, session: Session = Depends(db.session)):
-
     tokens = [row.token for row in session.query(PushTokens).filter_by(user_id=1).all() if row.token]
     print(tokens)
-    result = {
-        'to': tokens[0],
-        'sound': 'default',
-        'category': 'ALL',
-        'title': 'Today’s Bouquet 인터뷰',
-        'body': 'Bouquet에서 부캐들을 인터뷰하고 싶어해요! 나의 부캐에 대해 알려주세요!',
-        'data': 'https://forms.gle/d7eYq4y6pMXjBFd6A'
-    }
-
+    result = {'to': 'ExponentPushToken[2EqNf0ESFxv_Iq3-nvM2nU]', 'sound': 'default', 'category': 'all', 'title': 'Today’s Bouquet 인터뷰',
+              'body': 'Bouquet에서 부캐들을 인터뷰하고 싶어해요! 나의 부캐에 대해 알려주세요!',
+              'data': {'link': 'https://forms.gle/d7eYq4y6pMXjBFd6A'
+                       }
+              }
+    print(result)
     response = PushClient().publish(PushMessage(**result))
     response.validate_response()
